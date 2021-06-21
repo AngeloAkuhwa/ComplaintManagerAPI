97883f95944af3bf085d92c1dd513673f3ca2ede
AngeloAkuhwa <angeloakuhwa@gmail.com>
2021-06-21 14:45:46 +0100

    first push to complaint manager repo

---
+++ FacilityComplaintManager.Identity/Entity/AppUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FacilityComplaintManager.Identity.Entity
+{
+    public class AppUser:IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        //public Stack Stack { get; set; }
+        //public Squad Squad { get; set; }
+        //public ICollection<Complaint> Complains { get; set; }
+        //public ICollection<Reply> Replies { get; set; }
+    }
+}
